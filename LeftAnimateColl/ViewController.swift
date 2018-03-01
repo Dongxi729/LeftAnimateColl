b8fb86421ba4e9be3a9a0f04aa4354c4feb16590
zdx729 <18259129536@163.com>
2018-03-01 13:49:27 +0800

    Initial Commit

---
+++ LeftAnimateColl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeftAnimateColl
+//
+//  Created by 郑东喜 on 2018/3/1.
+//  Copyright © 2018年 郑东喜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
